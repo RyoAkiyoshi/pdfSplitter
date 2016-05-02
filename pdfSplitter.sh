7304432ffca75ce54197564da213ffff614f5d28
TOMITAHAYATO <tomitahayato@TOMITAHAYATO-no-Mac-mini.local>
2016-05-02 23:59:09 +0900

    作業用フォルダ内で作業する

--- pdfSplitter.sh
+++ pdfSplitter.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-convert test.pdf test.jpg
-convert -crop 50%x100% test.jpg output.jpg
-convert output-* dist.pdf
+
+#作業用のフォルダを作成
+mkdir intermedia
+convert test/test.pdf intermedia/test.jpg
+convert -crop 50%x100% intermedia/test.jpg intermedia/output.jpg
+convert intermedia/output-* dist.pdf
+#後片付け
+rm -rf intermedia
