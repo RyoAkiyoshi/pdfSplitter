acf115510c663b5a66f4196b7f600540640f2173
TOMITAHAYATO <tomitahayato@TOMITAHAYATO-no-Mac-mini.local>
2016-05-03 00:30:11 +0900

    iroiro

--- pdfSplitter.sh
+++ pdfSplitter.sh
@@ -2,8 +2,21 @@
 
 #作業用のフォルダを作成
 mkdir intermedia
-convert test/test.pdf intermedia/output.jpg
-convert -crop 50%x100% intermedia/output.jpg intermedia/split.jpg
-convert intermedia/split-* dist.pdf
+rootdir="/Users/tomitahayato/Desktop/pdfSplitter/"
+intermediadir=${rootdir}"intermedia/"
+rootfilepath=${rootdir}"test/*.pdf"
+
+#フォルダ内ファイル用の配列
+counter=0
+for file in ${rootfilepath}
+do
+    filearray+=("${file}")
+    convert ${file} ${intermediadir}output${counter}.jpg
+    convert -crop 50%x100% ${intermediadir}output${counter}.jpg ${intermediadir}split${counter}.jpg
+    counter=$(($counter+1))
+    echo "${counter} image is done"
+done
+
+convert ${intermediadir}split* dist.pdf
 #後片付け
-rm -rf intermedia
+#rm -rf intermedia
